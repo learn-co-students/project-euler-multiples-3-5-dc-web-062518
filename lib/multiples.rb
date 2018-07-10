4e2cec62bc64e40b5fec746544aaf804da7fbd08
github username <github email address>
2018-07-10 09:54:10 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+# def add_multiples_of_three_and_five(var)
+#   counter = 0
+#   (1...var).each do |i|
+#     if i % 3 == 0 || i % 5 == 0
+#     counter += i
+#     end
+#   end
+#   counter
+# end
+
+def collect_multiples(limit)
+  (1...limit).select do |i|
+    i % 3 == 0 || i % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  counter = 0
+  a = collect_multiples(limit)
+  a.each do |i|
+    counter += i
+  end
+  counter
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(num)
+    @limit = num
+  end
+
+  def collect_multiples
+    (1...@limit).select { |i|
+      i % 3 == 0 || i % 5 == 0}
+  end
+
+  def sum_multiples
+    a = self.collect_multiples
+    counter = 0
+    a.each do |i|
+      counter += i
+    end
+    counter
+  end
+
+end
