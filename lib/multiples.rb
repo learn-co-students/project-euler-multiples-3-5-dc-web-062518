3fd6eee909244804367c505403748747f6fb532d
JohnDaise <jdaise@gmail.com>
2018-07-10 10:03:55 -0400

    first commit

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+
+def collect_multiples(num)
+ (1...num).to_a.select{|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(num)
+ arr = collect_multiples(num)
+ arr.inject(0){|sum,n| sum + n}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+ attr_accessor :num
+ def initialize(num)
+   @num = num
+ end
+
+
+def collect_multiples
+  (1...self.num).to_a.select{|n| n % 3 == 0 || n % 5 == 0}
+end
+
+ def sum_multiples
+   arr = collect_multiples
+   arr.inject(0){|sum,n| sum + n}
+ end
+
+end
